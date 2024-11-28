494c6b185a989a6707f852224297e3b6c0470ad8
Nils Andreas Svee <me@lochnair.net>
2024-11-28 09:24:38 +0700

    chore: install bootstrap package so we can override colors by SASS

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,20 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    // Add this
+    css: {
+        preprocessorOptions: {
+            // if using SCSS
+            scss: {                                 
+                quietDeps: true,
+				silenceDeprecations: ['color-functions', 'global-builtin', 'import', 'legacy-js-api']
+            },
+            // if using SASS
+            sass: {                                 
+                quietDeps: true,
+				silenceDeprecations: ['color-functions', 'global-builtin', 'import', 'legacy-js-api']
+            },
+        },
+    }
 });
